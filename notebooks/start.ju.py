e28ed2ac8b3dcb72da77006c1293910cb6936f40
ziontee113 <ziontee113@gmail.com>
2023-11-01 14:41:56 +0700

    Add basic langchain chain

--- notebooks/start.ju.py
+++ notebooks/start.ju.py
@@ -4,14 +4,24 @@ from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
 # %%
 
 llm = Ollama(
-    model="zephyr", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+    model="zephyr",
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
 )
 
 # %%
 
-llm(
-    "create functions for a calculator in python. PLEASE make sure the function keyword is lowercased. DO NOT write any explanation"
+prompt = PromptTemplate(
+    template="Give me 5 interesting facts about {topic}", input_variables=["topic"]
 )
+
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+# %%
+
+chain.run("nudity in movies")
